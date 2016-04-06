67f37dd16c67c80d81aa7636dbc5556b7e715674
Matt Andrews <matthew.andrews02@bbc.co.uk>
2016-04-06 17:16:14 +0100

    Remove unused bits and comment gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var watchify = require('watchify');
 var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
@@ -15,15 +14,15 @@ var rev = require('gulp-rev');
 var revcssurls = require('gulp-rev-css-url');
 var revdel = require('gulp-rev-delete-original');
 var autoprefixer = require('gulp-autoprefixer');
-var gls = require('gulp-live-server');
 var nodemon = require('gulp-nodemon');
-var notify = require('gulp-notify');
 var argv = require('yargs').argv;
 var gulpif = require('gulp-if');
 
+// define main directories
 var inputBase = './assets/';
 var outputBase = './public/';
 
+// outline where static files live (or end up)
 var DIRS = {
     in: {
         root: inputBase,
@@ -39,6 +38,7 @@ var DIRS = {
     }
 };
 
+// define specific names for in/out files
 var FILES = {
     in: {
         js: 'main.js',
@@ -51,21 +51,26 @@ var FILES = {
     assets: 'assets.json'
 };
 
+// clean out folders of generated assets
 gulp.task('clean:css', function() { return del([DIRS.out.css + '/**/*']); });
 gulp.task('clean:js', function()  { return del([DIRS.out.js + '/**/*']); });
 gulp.task('clean:img', function() { return del([DIRS.out.img + '/**/*']); });
 gulp.task('clean:assets', function() { return del([DIRS.in.root + FILES.assets]); });
 
+// copy images to public dir
+// @TODO could minify them?
 gulp.task('copy:img', function() {
     gulp.src(DIRS.in.img + '/**/*').pipe(gulp.dest(DIRS.out.img));
 });
 
+// compile JS (after cleaning them out)
 gulp.task('js', ['clean:assets', 'clean:js'], function() {
     var b = browserify({
         entries: [DIRS.in.js + '/' + FILES.in.js],
         debug: true
     });
 
+    // minify on LIVE, sourcemap on DEV
     return b.bundle()
         .pipe(source(FILES.out.js))
         .pipe(buffer())
@@ -77,6 +82,9 @@ gulp.task('js', ['clean:assets', 'clean:js'], function() {
         .pipe(livereload());
 });
 
+// compile Sass (after cleaning and moving images)
+// references to images will be replaced (on --production/LIVE)
+// with cachebusted versions
 gulp.task('sass', ['clean:assets', 'clean:css', 'clean:img', 'copy:img'], function() {
     return gulp.src(DIRS.in.css + '/' + FILES.in.css)
         .pipe(gulpif(!argv.production, sourcemaps.init()))
@@ -92,6 +100,8 @@ gulp.task('sass', ['clean:assets', 'clean:css', 'clean:img', 'copy:img'], functi
         .pipe(livereload());
 });
 
+// hash/version static files and replace image paths in CSS
+// then write an output file (only on LIVE)
 gulp.task('rev', ['sass', 'js'], function() {
     return gulp.src(DIRS.out.root + '/**/*')
         .pipe(rev())
@@ -102,11 +112,8 @@ gulp.task('rev', ['sass', 'js'], function() {
         .pipe(gulp.dest(DIRS.in.root));
 });
 
-gulp.task('serve', ['watch'], function() {
-    var server = gls.new('server.js');
-    server.start();
-});
-
+// a dev server which reloads express when server-side JS changes
+// and watches for changes to client-side code
 gulp.task('server', ['watch'], function() {
     nodemon({
         script: 'server.js',
@@ -121,12 +128,16 @@ gulp.task('server', ['watch'], function() {
     });
 });
 
-
+// dev task: start the server and watch for changes
 gulp.task('default', ['clean:assets', 'dev', 'server']);
 
+// used on LIVE (add --production flag to uglify files)
 gulp.task('build', ['sass', 'js', 'rev']);
+
+// used for local dev testing (doesn't version files)
 gulp.task('dev', ['sass', 'js']);
 
+// watch static files for changes and recompile
 gulp.task('watch', function() {
     livereload.listen();
     gulp.watch(DIRS.in.css + '/**/*.scss', ['sass']);
